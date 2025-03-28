aebdcc3efdcb2ef75b6425c833978648b2189144
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-28 10:44:09 +0530

    Update Abhi.py

--- Abhi.py
+++ Abhi.py
@@ -1,6 +1,6 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.raw.functions.messages import GetStickerSet, GetDocumentById
+from pyrogram.raw.functions.messages import GetStickerSet, GetDocumentByHash
 from pyrogram.raw.types import InputStickerSetShortName
 from pyrogram.types import Message, InputMediaDocument
 
